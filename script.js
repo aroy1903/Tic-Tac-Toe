706458f06c43205db3cc010b40458c5e1f061af9
aroy1903 <ananthroy1903@gmail.com>
2022-09-12 01:45:25 -0700

    new winCheck

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const gameScreen = document.querySelector("#gameBoard");
-const playButton = document.querySelector("#playButton");
-const playerHeader = document.querySelector("#playerTurn");
-const playScreen = document.querySelector("#playScreen");
+const gameScreen = document.querySelector('#gameBoard');
+const playButton = document.querySelector('#playButton');
+const playerHeader = document.querySelector('#playerTurn');
+const playScreen = document.querySelector('#playScreen');
 
 const gameGrid = [];
 var turn = 1;
@@ -9,31 +9,45 @@ const player = (name, symbol) => {
   const printOutput = () => console.log(`${name},${symbol}`);
   return { name, symbol, printOutput };
 };
-const playerX = player("Player X's", "X");
-const playerO = player("Player O's", "O");
+const playerX = player("Player X's", 'X');
+const playerO = player("Player O's", 'O');
 
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const playerInputs = ['', '', '', '', '', '', '', '', ''];
 const gameBoard = (() => {
   const gameBlocks = () => {
     for (i = 1; i <= 9; i++) {
-      let gameBlock = document.createElement("div");
-      gameBlock.className = "gameBlock";
+      let gameBlock = document.createElement('div');
+      gameBlock.className = 'gameBlock';
       gameGrid.push(gameBlock);
     }
     for (const block of gameGrid) {
+      const blockIndex = gameGrid.indexOf(block);
       gameScreen.appendChild(block);
-      block.addEventListener("click", () => {
-        if (block.innerText !== "") {
-          return alert("Choose Another Box");
+      block.addEventListener('click', () => {
+        if (block.innerText !== '') {
+          return alert('Choose Another Box');
         }
         if (turn % 2 !== 0) {
           playerHeader.innerText = `${playerO.name} turn.`;
+          playerInputs[blockIndex] = 'X';
           block.innerText = playerX.symbol;
         } else if (turn % 2 === 0) {
           playerHeader.innerText = `${playerX.name} turn.`;
+          playerInputs[blockIndex] = 'O';
           block.innerText = playerO.symbol;
         }
-        checkWin();
-
+        checkWinner();
         turn += 1;
       });
     }
@@ -41,181 +55,83 @@ const gameBoard = (() => {
   return { gameBlocks };
 })();
 
-playButton.addEventListener("click", () => {
-  playerHeader.style.display = "block";
+playButton.addEventListener('click', () => {
+  playerHeader.style.display = 'block';
   gameScreen.removeChild(playScreen);
   gameBoard.gameBlocks();
 });
 let to = 150;
-function checkWin() {
-  if (
-    gameGrid[0].innerText === "X" &&
-    gameGrid[1].innerText === "X" &&
-    gameGrid[2].innerText === "X"
-  ) {
-    setTimeout(() => winScreen(playerX.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[0].innerText === "O" &&
-    gameGrid[1].innerText === "O" &&
-    gameGrid[2].innerText === "O"
-  ) {
-    setTimeout(() => winScreen(playerO.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[3].innerText === "X" &&
-    gameGrid[4].innerText === "X" &&
-    gameGrid[5].innerText === "X"
-  ) {
-    setTimeout(() => winScreen(playerX.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[3].innerText === "O" &&
-    gameGrid[4].innerText === "O" &&
-    gameGrid[5].innerText === "O"
-  ) {
-    setTimeout(() => winScreen(playerO.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[6].innerText === "X" &&
-    gameGrid[7].innerText === "X" &&
-    gameGrid[8].innerText === "X"
-  ) {
-    setTimeout(() => winScreen(playerX.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[6].innerText === "O" &&
-    gameGrid[7].innerText === "O" &&
-    gameGrid[8].innerText === "O"
-  ) {
-    setTimeout(() => winScreen(playerO.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[0].innerText === "X" &&
-    gameGrid[3].innerText === "X" &&
-    gameGrid[6].innerText === "X"
-  ) {
-    setTimeout(() => winScreen(playerX.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[0].innerText === "O" &&
-    gameGrid[3].innerText === "O" &&
-    gameGrid[6].innerText === "O"
-  ) {
-    setTimeout(() => winScreen(playerO.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[1].innerText === "X" &&
-    gameGrid[4].innerText === "X" &&
-    gameGrid[7].innerText === "X"
-  ) {
-    setTimeout(() => winScreen(playerX.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[1].innerText === "O" &&
-    gameGrid[4].innerText === "O" &&
-    gameGrid[7].innerText === "O"
-  ) {
-    setTimeout(() => winScreen(playerO.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[2].innerText === "X" &&
-    gameGrid[5].innerText === "X" &&
-    gameGrid[8].innerText === "X"
-  ) {
-    setTimeout(() => winScreen(playerX.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[2].innerText === "O" &&
-    gameGrid[5].innerText === "O" &&
-    gameGrid[8].innerText === "O"
-  ) {
-    setTimeout(() => winScreen(playerO.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[0].innerText === "X" &&
-    gameGrid[4].innerText === "X" &&
-    gameGrid[8].innerText === "X"
-  ) {
-    setTimeout(() => winScreen(playerX.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[0].innerText === "O" &&
-    gameGrid[4].innerText === "O" &&
-    gameGrid[8].innerText === "O"
-  ) {
-    setTimeout(() => winScreen(playerO.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[2].innerText === "X" &&
-    gameGrid[4].innerText === "X" &&
-    gameGrid[6].innerText === "X"
-  ) {
-    setTimeout(() => winScreen(playerX.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[2].innerText === "O" &&
-    gameGrid[4].innerText === "O" &&
-    gameGrid[6].innerText === "O"
-  ) {
-    setTimeout(() => winScreen(playerO.name), to);
-    console.log("win");
-  } else if (
-    gameGrid[0].innerText !== "" &&
-    gameGrid[1].innerText !== "" &&
-    gameGrid[2].innerText !== "" &&
-    gameGrid[3].innerText !== "" &&
-    gameGrid[4].innerText !== "" &&
-    gameGrid[5].innerText !== "" &&
-    gameGrid[6].innerText !== "" &&
-    gameGrid[7].innerText !== "" &&
-    gameGrid[8].innerText !== ""
-  ) {
-    setTimeout(() => drawScreen(), 250);
-    console.log("draw");
+
+const checkWinner = () => {
+  let roundWond = false;
+  for (i = 0; i < winConditions.length; i++) {
+    const newArr = winConditions[i];
+    const a = playerInputs[newArr[0]];
+    const b = playerInputs[newArr[1]];
+    const c = playerInputs[newArr[2]];
+
+    if (a == '' || b == '' || c == '') {
+      continue;
+    }
+    if (a == b && b == c) {
+      if (a == 'X' && b == 'X') {
+        setTimeout(() => winScreen(playerX.name), to);
+        roundWond = true;
+        break;
+      } else if (a == 'O' && b == 'O') {
+        setTimeout(() => winScreen(playerO.name), to);
+        roundWond = true;
+        break;
+      }
+    }
   }
-}
+  if (roundWond) {
+    return;
+  } else if (!playerInputs.includes('')) {
+    setTimeout(() => drawScreen(), to);
+  }
+};
 
 function winScreen(name) {
-  const gameBlockClass = document.querySelectorAll(".gameBlock");
-  playerHeader.style.display = "none";
+  const gameBlockClass = document.querySelectorAll('.gameBlock');
+  playerHeader.style.display = 'none';
   for (let blck of gameBlockClass) {
     gameScreen.removeChild(blck);
   }
-  let winner = document.createElement("div");
-  let Pbuton = document.createElement("button");
-  let winHeader = document.createElement("h1");
+  let winner = document.createElement('div');
+  let Pbuton = document.createElement('button');
+  let winHeader = document.createElement('h1');
   winHeader.innerText = `${name} win!👑`;
-  Pbuton.innerText = "Play Again";
-  Pbuton.setAttribute("id", "playAgainButton");
-  Pbuton.addEventListener("click", () => {
+  Pbuton.innerText = 'Play Again';
+  Pbuton.setAttribute('id', 'playAgainButton');
+  Pbuton.addEventListener('click', () => {
     gameScreen.removeChild(winner);
     window.location.reload();
   });
-  winHeader.setAttribute("id", "winnerHeader");
-  winner.className = "winScreen";
+  winHeader.setAttribute('id', 'winnerHeader');
+  winner.className = 'winScreen';
   gameScreen.appendChild(winner);
   winner.appendChild(winHeader);
   winner.appendChild(Pbuton);
 }
 function drawScreen() {
-  const gameBlockClass = document.querySelectorAll(".gameBlock");
-  playerHeader.style.display = "none";
+  const gameBlockClass = document.querySelectorAll('.gameBlock');
+  playerHeader.style.display = 'none';
   for (let blck of gameBlockClass) {
     setTimeout(gameScreen.removeChild(blck), 5000);
   }
-  let winner = document.createElement("div");
-  let Pbuton = document.createElement("button");
-  let winHeader = document.createElement("h1");
+  let winner = document.createElement('div');
+  let Pbuton = document.createElement('button');
+  let winHeader = document.createElement('h1');
   winHeader.innerText = `Draw`;
-  Pbuton.innerText = "Play Again";
-  Pbuton.setAttribute("id", "playAgainButton");
-  Pbuton.addEventListener("click", () => {
+  Pbuton.innerText = 'Play Again';
+  Pbuton.setAttribute('id', 'playAgainButton');
+  Pbuton.addEventListener('click', () => {
     gameScreen.removeChild(winner);
     window.location.reload();
   });
-  winHeader.setAttribute("id", "winnerHeader");
-  winner.className = "winScreen";
+  winHeader.setAttribute('id', 'winnerHeader');
+  winner.className = 'winScreen';
   gameScreen.appendChild(winner);
   winner.appendChild(winHeader);
   winner.appendChild(Pbuton);
